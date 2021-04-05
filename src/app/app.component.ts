22423528d86538c06c21ea6fffc6664a1faa9eb1
GowthamanM-Neo <gowthaman@iamneo.ai>
2021-04-05 16:38:45 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { City } from 'src/model/city';
+import { ApiService } from 'src/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ang-Project-XIII';
+  database:any={};
+  userName:any;
+  users:any;
+  countries:any;
+  cities:any;
+  city:any = {};
+  countryName:any;
+  cityName:any;
+  genderValue:any;
+  constructor(private apiService:ApiService){
+    this.apiService.getCountries().subscribe((data)=>{
+      this.countries = data;
+    });
+
+    this.apiService.getUsers().subscribe((data)=>{
+      this.database=data;
+    })
+  }
+  getCity(){
+    this.city.country = this.countryName;
+    this.apiService.getCities(this.city)
+    .subscribe((data)=>{
+      this.cities = data.data;
+    });
+  }
+
+  submit(){
+    console.log(this.userName);
+    console.log(this.database);
+    this.users = this.database.filter((data: 
+      { firstName: any;
+        country:any;
+        city:any;
+        gender:any;
+      }) => (data.firstName === this.userName && data.country === this.countryName.toLowerCase() && data.city === this.cityName.toLowerCase() && data.gender === this.genderValue));
+    console.log(this.users);
+  }
 }
